d4f3c062ed224bb30570fd51c2dbf1be89083345
Amoo Richard <amreechy@gmail.com>
2025-08-02 09:55:40 +0100

    Initial commit - Django + Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		'./templates/**/*.html',
+    './**/templates/**/*.html',
+    './static/css/src/**/*.css',
+    './static/js/**/*.js',
+	],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
